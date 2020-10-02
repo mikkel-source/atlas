73f6d0d5e2e45ba74a988d3e9de8d68604c5a1b2
mikkel <6107577@rts-365.dk>
2020-10-02 13:23:15 +0200

    change

--- flag.js
+++ flag.js
@@ -1,18 +1,12 @@
-let url = new URLSearchParams(window.location.search)
-console.log(url)
-if (url.has("id")) {
-    console.log(url.get("id"))
-    fetch("https://restcountries.eu/rest/v2/all")
-        .then(res => res.json())
-        .then(function (data) {
-            //console.log(data)
-            let flaglist = document.querySelector(".flaglist")
-            data.forEach(function (country) {
-                console.log(country.flag)
-                let template = document.querySelector("#template").content.cloneNode(true)
-                let image = template.querySelector("img")
-                image.src = country.flag
-                flaglist.appendChild(image)
-            });
-        })
-};
+fetch("https://restcountries.eu/rest/v2/all")
+    .then(res => res.json())
+    .then(function (data) {
+        let flaglist = document.querySelector(".flaglist")
+        data.forEach(function (country) {
+            let template = document.querySelector("#template").content.cloneNode(true)
+            let image = template.querySelector("img")
+            image.src = country.flag
+            flaglist.appendChild(image)
+        });
+    })
+    ;
---
+++ router.js
@@ -0,0 +1,22 @@
+function router() {
+    let url = new URLSearchParams(window.location.search);
+    console.log(url.get)
+    switch (url.get("page")) {
+        case "home":
+
+
+            break;
+        case "about":
+
+
+            break;
+        case "flag":
+
+    }
+
+}
+
+
+
+
+
